81d5bef16d6494c00f7f10c8a5c07b5b40ae603e
iaguirre6 <84925523+iaguirre6@users.noreply.github.com>
2022-01-10 17:03:38 -0500

    [DEV] creation of warnings.py
    [DEV] warnings.py is a collection of custom warnings to be used throughout the package

---
+++ snapReactors/functions/warnings.py
@@ -0,0 +1,20 @@
+"""warnings
+
+collection of custom warnings for different uses through the package.
+
+Created on 2022-01-10 16:56:20 @author: Isaac Naupa
+Last updated on 2022-01-10 16:56:28 @author: Isaac Naupa
+email: iaguirre6@gatech.edu
+"""
+
+class InputFileSyntaxWarning(UserWarning):
+    """InputFileWarning
+
+    Warning to be used when dealing with inputfile syntax warnings.
+    The warning can be used by importing
+
+    from serpentGenerator.functions.warnings import InputFileSyntaxWarning
+
+    warning.warn("warning message", InputFileSyntaxWarning)
+    """
+    pass
