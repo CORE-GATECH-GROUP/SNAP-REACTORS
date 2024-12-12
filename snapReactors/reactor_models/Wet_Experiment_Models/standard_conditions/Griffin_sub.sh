5ef5ec06bb2d82752898606e23d79fd3b00b8d05
Garcia <garcsamu@sawtooth1.ib0.sawtooth.hpc.inl.gov>
2024-12-12 13:19:08 -0700

    [DEV] First griffin run complete, now need to incorporate multiphysics

--- snapReactors/reactor_models/Wet_Experiment_Models/standard_conditions/Griffin_sub.sh
+++ snapReactors/reactor_models/Wet_Experiment_Models/standard_conditions/Griffin_sub.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#PBS -l select=4:ncpus=10:mpiprocs=10
+#PBS -l select=4:ncpus=40:mpiprocs=1:mem=160GB
 #PBS -N Griffin
-#PBS -l walltime=00:40:00
+#PBS -l walltime=01:40:00
 #PBS -k doe
 #PBS -j oe
 #PBS -P edu_res
  
 cat $PBS_NODEFILE
-module load use.exp_ctl
-module load use.moose 
-module load griffin-openmpi
+module load use.exp_ctl use.moose moose-apps griffin
 cd $PBS_O_WORKDIR
 export TMPDIR=/tmp
 mpiexec -n 40 griffin-opt -i /home/garcsamu/Serpent/SNAP-REACTORS-PRIVATE/snapReactors/reactor_models/Wet_Experiment_Models/standard_conditions/wet_core.i
