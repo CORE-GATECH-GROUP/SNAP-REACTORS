6a034dcbbef42c80d971298cbcee952fde87ae52
Garcia <garcsamu@hpclogin.hpc.inl.gov>
2025-02-03 07:57:27 -0700

    [DEV] burnup tabulation scripting complete, will move in XS table when completed

--- snapReactors/reactor_models/Wet_Experiment_Models/standard_conditions/array_sub_burn.sh
+++ snapReactors/reactor_models/Wet_Experiment_Models/standard_conditions/array_sub_burn.sh
@@ -4,17 +4,18 @@
 fuel_temps=(1 2 3)        # Adjust these as needed for the number of fuel temperature cases
 coolant_temps=(1 2 3)     # Adjust these as needed for the number of coolant temperature cases
 reflector_temps=(1 2 3)   # Adjust these as needed for the number of reflector temperature cases
-
+burnup_steps=(1 2 3)
 # Loop through all combinations of fuel, coolant, and reflector indices
-for i in "${fuel_temps[@]}"; do
-    for j in "${coolant_temps[@]}"; do
-        for k in "${reflector_temps[@]}"; do
+for l in "${burnup_steps[@]}"; do
+    for i in "${fuel_temps[@]}"; do
+        for j in "${coolant_temps[@]}"; do
+            for k in "${reflector_temps[@]}"; do
             # Create a PBS script for each job
-            cat << EOF > submit_job_${i}_${j}_${k}.pbs
+            cat << EOF > submit_job_${l}_${i}_${j}_${k}.pbs
 #!/bin/bash
-#PBS -l select=8:ncpus=40:mpiprocs=1
-#PBS -N SerpentJob_${i}_${j}_${k}
-#PBS -l walltime=2:30:00
+#PBS -l select=8:ncpus=40:mpiprocs=1:mem=200gb
+#PBS -N SerpentJob_${l}_${i}_${j}_${k}
+#PBS -l walltime=00:45:00
 #PBS -k doe
 #PBS -j oe
 #PBS -P edu_res
@@ -30,15 +31,17 @@ export TMPDIR=/tmp
 export SERPENT_DATA=/hpc-common/data/serpent/xsdata/s2v0_endfb80
 
 # Run Serpent job with 4 nodes and 40 CPUs per node
-mpirun sss2 s82d_ac_c3_gcu_ringres_${i}_${j}_${k}.main -omp 40
+mpirun sss2 s82d_ac_c3_gcu_ringres_${l}_${i}_${j}_${k}.main -omp 40
 EOF
 
             # Submit the job
-            qsub submit_job_${i}_${j}_${k}.pbs
+            qsub submit_job_${l}_${i}_${j}_${k}.pbs
             
             # Delete the PBS script after submission
-            rm submit_job_${i}_${j}_${k}.pbs
+            rm submit_job_${l}_${i}_${j}_${k}.pbs
+            done
         done
     done
 done
+
 
